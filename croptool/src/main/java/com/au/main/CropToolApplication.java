76cc8d8287c1bc1f5488e85b6009d113e2a03de3
ajay <ajaysraja047@gmail.com>
2021-02-07 22:46:28 +0530

    PPT resources + swagger added

--- croptool/src/main/java/com/au/main/CropToolApplication.java
+++ croptool/src/main/java/com/au/main/CropToolApplication.java
@@ -2,8 +2,10 @@ package com.au.main;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import springfox.documentation.swagger2.annotations.EnableSwagger2;
 
 @SpringBootApplication
+@EnableSwagger2
 public class CropToolApplication {
 
 	public static void main(String[] args) {
